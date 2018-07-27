cd07e739831b768833a30f52168f5778d5094c1b
Justin Go <jz10@jz10.info>
2018-07-28 02:52:05 +0800

    feat: add additional route validation

--- routes.js
+++ routes.js
@@ -1,4 +1,3 @@
-const debug = require('debug')('routes.js')
 const express = require('express');
 const Joi = require('joi');
 const ev = require('express-validation');
@@ -10,35 +9,105 @@ const reportsCtrl = require('./controllers/reports.controller.js');
 const categoryCtrl = require('./controllers/category.controller.js');
 
 // expenses routes
-router.route('/expenses')
-  .post(expenseCtrl.createOne)
-  .get(expenseCtrl.fetchAll)
-router.route('/expenses/:category_id')
-  .get(expenseCtrl.fetchOne)
-  .put(expenseCtrl.updateOne)
-  .delete(expenseCtrl.deleteOne);
-
+router
+  .route('/expenses')
+  .post(
+    ev({
+      body: {
+        category_id: Joi.string(),
+        title: Joi.string().required(),
+        date: Joi.string().required(),
+        value: Joi.number().required(),
+      },
+    }),
+    expenseCtrl.createOne
+  )
+  .get(expenseCtrl.fetchAll);
+router
+  .route('/expenses/:expense_id')
+  .get(
+    ev({
+      params: {
+        expense_id: Joi.string().required(),
+      },
+    }),
+    expenseCtrl.fetchOne
+  )
+  .put(
+    ev({
+      params: {
+        expense_id: Joi.string().required(),
+      },
+      body: {
+        category_id: Joi.string(),
+        title: Joi.string().required(),
+        date: Joi.string().required(),
+        value: Joi.number().required(),
+      },
+    }),
+    expenseCtrl.updateOne
+  )
+  .delete(
+    ev({
+      params: {
+        expense_id: Joi.string().required(),
+      },
+    }),
+    expenseCtrl.deleteOne
+  );
 
 // reports routes
-router.route('/reports')
-  .get(reportsCtrl.fetchAll)
-  .get(reportsCtrl.fetchOne)
-  .put(reportsCtrl.updateOne)
-  .delete(reportsCtrl.deleteOne);
-
+router
+  .route('/reports')
+  .get(reportsCtrl.fetchAll);
 
 // categories routes
-router.route('/categories')
-  .post(categoryCtrl.createOne)
-  .get(categoryCtrl.fetchAll)
-router.route('/categories/:category_id')
-  .get(categoryCtrl.fetchOne)
-  .put(categoryCtrl.updateOne)
-  .delete(categoryCtrl.deleteOne);
+router
+  .route('/categories')
+  .post(
+    ev({
+      body: {
+        description: Joi.string(),
+        title: Joi.string().required(),
+      },
+    }),
+    categoryCtrl.createOne
+  )
+  .get(categoryCtrl.fetchAll);
 
+router
+  .route('/categories/:category_id')
+  .get(
+    ev({
+      params: {
+        category_id: Joi.string().required(),
+      },
+    }),
+    categoryCtrl.fetchOne
+  )
+  .put(
+    ev({
+      params: {
+        category_id: Joi.string().required(),
+      },
+      body: {
+        description: Joi.string(),
+        title: Joi.string(),
+      },
+    }),
+    categoryCtrl.updateOne
+  )
+  .delete(
+    ev({
+      params: {
+        category_id: Joi.string().required(),
+      },
+    }),
+    categoryCtrl.deleteOne
+  );
 
-
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   logError(JSON.stringify(err.stack || err, null, 2));
 
   if (err instanceof ev.ValidationError) {
